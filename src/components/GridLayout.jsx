6f2db6d4b7fbe1bbae8c158672de6048e88b8e05
Ariel_Naiman <a0527633600@gmail.com>
2026-02-24 09:05:45 +0200

    commit

--- src/components/GridLayout.jsx
+++ src/components/GridLayout.jsx
@@ -1,32 +1,37 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import "../styles/GridLayout.css"
 
 export default function GridLayout() {
 
   const [counter, setCounter] = useState(0)
   const [textStatus, setTextStatus] = useState("")
-  const [status, setStatus] = useState("")
+  const [status, setStatus] = useState("status-false")
   const cecret = useRef(Math.floor(Math.random() * 100) + 1)
-  console.log(cecret);
-
+  // const color = useRef(randomColor())
 
   const board = []
   for (let i = 1; i < 101; i++) {
     board.push(i)
   }
 
+  // function randomColor() {
+  //   const r = Math.floor(Math.random() * 256)
+  //   const g = Math.floor(Math.random() * 256)
+  //   const b = Math.floor(Math.random() * 256)
+  //   return `rgb(${r}, ${g}, ${b})`
+  // }
+
   function onClick(event, key) {
     if (event.target.className === "x") return
     if (key != cecret.current) {
       setCounter(counter + 1)
-      event.target.className = 'cell err'
+      event.target.className = 'x'
       setStatus("status-false")
       setTextStatus("Keep Searching!")
-      setTimeout(() => {
-        event.target.className = 'x'
-        setTextStatus("")
-      }, [600])
+      event.target.textContent = 'X'
+      setTimeout(() => setTextStatus(""), [1000])
     } else {
+      // event.target.style.backgroundColor = randomColor()
       setStatus("status-true")
       setTextStatus("You have a victory! 🤩")
       event.target.className = 'cell v'
@@ -35,7 +40,7 @@ export default function GridLayout() {
 
   return (
     <div className="main">
-      <h1 className="title">Find the secret color!</h1>
+      <h1 className="title">Find the secret color! <div className="divColor" style={{ backgroundColor: "greenyellow" }}></div></h1>
       <div className="table">
         {board.map((num) => (
           <div key={num} className={num === cecret ? "secret" : "cell"} onClick={e => {
@@ -46,6 +51,7 @@ export default function GridLayout() {
       </div>
       <p className="click" >Clicks: {counter}</p>
       <p className={status}>{textStatus}</p>
+      <button type="submit" className={`${status}-button`} >New Game</button>
     </div>
   )
 }
